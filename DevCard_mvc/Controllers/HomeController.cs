01f8e7879388c8c0502ccd16dfe0c27ddf48336b
MAXELL <MAXELL@DESKTOP-3TM615P>
2022-01-30 10:57:14 +0330

    index & contact page added

--- DevCard_mvc/Controllers/HomeController.cs
+++ DevCard_mvc/Controllers/HomeController.cs
@@ -1,29 +1,22 @@
 ﻿using DevCard_mvc.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DevCard_mvc.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+            
         }
-
-        public IActionResult Index()
+        public IActionResult Contact()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public IActionResult Index()
         {
             return View();
         }
